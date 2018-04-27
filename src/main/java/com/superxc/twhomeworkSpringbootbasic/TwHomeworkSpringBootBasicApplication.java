cae685b95c389f2c1f2cfa99827360cb8600a153
superxc <superxcgm@gmail.com>
2018-04-27 09:04:13 +0800

    add spring boot project file

---
+++ src/main/java/com/superxc/twhomeworkSpringbootbasic/TwHomeworkSpringBootBasicApplication.java
@@ -0,0 +1,12 @@
+package com.superxc.twhomeworkSpringbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwHomeworkSpringBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwHomeworkSpringBootBasicApplication.class, args);
+	}
+}
